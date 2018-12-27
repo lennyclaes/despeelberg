bffa64829b8f8592c4d5a37b84c11d1d0cfe69ec
Lenny Claes <lenny.claes@gmail.com>
2018-12-27 22:47:06 +0100

    Initial commit

---
+++ js/slideshow.js
@@ -0,0 +1,43 @@
+(() => {
+    let images = [
+        "bg1.webp",
+        "bg2.webp",
+        "bg3.webp"
+    ];
+
+    let imageElts = [];
+
+    let curImg = 0;
+    let elt = document.querySelector('#slideshow');
+
+    function loadImages() {
+        images.forEach(image => {
+            let img = document.createElement('IMG');
+            img.classList.add('slide');
+            img.style.display = 'none';
+            img.setAttribute('src', `/img/slideshow/${image}`);
+            imageElts.push(img);
+            elt.appendChild(img);
+        });
+        slideshow();
+    }
+
+    function slideshow() {
+        imageElts[curImg].style.display = 'block';
+        curImg++;
+        setInterval(nextSlide, 4000);
+    }
+
+    function nextSlide() {
+        imageElts.forEach(img => {
+            img.style.display = 'none';
+        })
+        imageElts[curImg].style.display = 'block';
+        curImg++;
+        if(curImg >= images.length) {
+            curImg = 0;
+        }
+    }
+
+    loadImages();
+})();
